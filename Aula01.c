abb9cd56c55cf9f7a2fdb1f2a9ecab26c5b5e8bf
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2024-11-27 21:57:00 -0300

    .

---
+++ Aula01.c
@@ -0,0 +1,17 @@
+// Variáveis
+#include <stdio.h>
+
+int main() { // A chave delimita um bloco de código (Incio do bloco)
+    // Declarando variável
+    int idade;
+
+    // Função que imprime algo na tela
+    printf("Ola, Mundo\n"); // O ponto e virgula finaliza um comando
+
+    // Recebe os dados
+    scanf("%d", &idade);
+
+    printf("A sua idade é = %d", idade);
+
+    return 0;
+} // Fim do bloco
