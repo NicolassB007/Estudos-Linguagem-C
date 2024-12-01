1f5e08614896a95401e214f82ee0e7f477e9cf1b
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2024-12-01 11:42:30 -0300

    .

--- Exercicios/Ex24.c
+++ Exercicios/Ex24.c
@@ -3,7 +3,33 @@
 #include <stdio.h>
 
 int main() {
-    int i, j, m, n;
+    int i, j, m, n, multi_matA;
+    printf("Informe a quantidade de linhas da matriz A: ");
+    scanf("%d", &m);
+    printf("Informe a quantidade de colunas da matriz A: ");
+    scanf("%d", &n);
+
+    int mat[m][n];
+
+    printf("\n");
+
+    printf("MATRIZ A\n");
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            printf("Elemento [%d, %d]: ", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    printf("MULTIPLICACAO MATRIZ A\n");
+    multi_matA = 0;
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            multi_matA = multi_matA * mat[i][j];
+        }
+    }
+
+    printf("RESULTADO DA MATRIZ A = %d\n", multi_matA);
 
     return 0;
 }
