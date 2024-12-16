1c218b7d3375fa895eeced9953e8567a61564ce5
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2024-12-16 11:27:07 -0300

    ..

--- Exercicios-do-Sid/Exercicios-Escolha-Caso/Ex11.c
+++ Exercicios-do-Sid/Exercicios-Escolha-Caso/Ex11.c
@@ -21,34 +21,42 @@ void tracos_inferiores() {
 }
 
 int main() {
+    setlocale(LC_ALL, "Portuguese_Brazil");
     char nome_cliente[20];
-    int i, numero_conta, operacao;
+    int i, numero_conta, operacao, contador;
     double saldo_bancario;
     char espacos = {' '};
 
-    tracos_superior();
-    printf("%-15c CONTA CORRENTE", espacos);
-    tracos_inferiores();
+    contador = 1;
 
-    printf("Qual o nome cliente: ");
-    fgets(nome_cliente, 20, stdin);
-    printf("Qual o número da conta: ");
-    scanf("%d", &numero_conta);
-    printf("Qual o saldo bancario: ");
-    scanf("%lf", &saldo_bancario);
+    do {
+        tracos_superior();
+        printf("%-15c CONTA CORRENTE", espacos);
+        tracos_inferiores();
 
-    tracos_superior();
-    printf("NOME DO CLIENTE -> %s", nome_cliente);
-    printf("NUMERO DA CONTA -> %d\n", numero_conta);
-    printf("SALDO DA CONTA -> %.2lf", saldo_bancario);
-    tracos_inferiores();
+        printf("Qual o nome cliente: ");
+        fgets(nome_cliente, 20, stdin);
+        printf("Qual o número da conta: ");
+        scanf("%d", &numero_conta);
+        printf("Qual o saldo bancário: ");
+        scanf("%lf", &saldo_bancario);
 
-    tracos_superior();
-    printf("%-15c FUNCIONALIDADES", espacos);
-    tracos_inferiores();
-    printf("[ 1 ] -> DEPOSITO\n[ 2 ] -> RETIRADA\n");
-    printf("Qual operacao sera efetuada: ");
-    scanf("%d", &operacao);
+        tracos_superior();
+        printf("NOME DO CLIENTE -> %s", nome_cliente);
+        printf("NUMERO DA CONTA -> %d\n", numero_conta);
+        printf("SALDO DA CONTA -> %.2lf", saldo_bancario);
+        tracos_inferiores();
+
+        tracos_superior();
+        printf("%-15c FUNCIONALIDADES", espacos);
+        tracos_inferiores();
+        printf("[ 1 ] -> DEPOSITO\n[ 2 ] -> RETIRADA\n[ 3 ] -> SAIR\n");
+        printf("Qual operação será efetuada: ");
+        scanf("%d", &operacao);
+        fflush(stdin);
+        contador = contador + 1;
+        printf("CONTADOR = %d\n\n", contador);
+    } while (operacao >= 1 && operacao < 3);
 
     return 0;
 }
